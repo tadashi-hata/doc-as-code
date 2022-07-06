8200d6574182fa904db2188f69517fdf4f0ab102
Hata <tiha@gft.com>
2022-07-06 10:08:03 -0300

    Initial setup with markdoc and markdoc js dependency

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withMarkdoc = require('@markdoc/next.js');
+
+module.exports = withMarkdoc(/* options */)({
+  pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx']
+});
---
+++ pages/docs/markdown-example.md
@@ -0,0 +1,18 @@
+# Testing markdocs
+Random text here
+
+> Info
+> Whatever works for you, works for me too
+
+| Testing | Table | 
+|---------|-------|
+| item    | tic tac |
+| item    | foo |
+
+--- 
+
+## Another Header here
+
+foo bar
+
+            
